3b4a02763e182497268c382d77ea7a5de8bac831
thando mini <tzone85@gmail.com>
2019-10-02 21:51:37 +0200

    validating and sanitizing the input separately

--- filters.php
+++ filters.php
@@ -7,6 +7,13 @@
     }
 
     if (filter_has_var(INPUT_POST, 'data')) {
+
+        $email = $_POST['data'];
+
+        // Remove illegal chars
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        echo $email.'<br>';
+
         if (filter_input(INPUT_POST, 'data', FILTER_VALIDATE_EMAIL)) {
             echo 'Email is valid';
         } else {
@@ -15,7 +22,7 @@
     }
 ?>
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
     <input type="text" name="data">
     <button type="submit">Submit</button>
 </form>
